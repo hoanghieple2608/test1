ec75fde94fc2ac60179acec461f167c440caf05f
hoanghieple2608 <56873627+hoanghieple2608@users.noreply.github.com>
2020-05-16 14:57:31 +0700

    Add files via upload

---
+++ Lê_Hoàng_Hiệp_PHP_CB/db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE shop;
+
+CREATE TABLE quanlymathang (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      ten VARCHAR (255) NOT NULL,
+      nsx VARCHAR (255) NOT NULL,
+      nd VARCHAR (255) NOT NULL ,
+      gia VARCHAR (255) NOT NULL ,
+      soluong VARCHAR (255) NOT NULL ,
+);
